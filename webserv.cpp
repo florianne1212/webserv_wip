7c091d98e8fb549a16873f61e99dd8149735a087
user42 <fcoudert@student.42.fr>
2021-05-27 15:52:58 +0200

    basic

---
+++ webserv.cpp
@@ -0,0 +1,58 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int fd_server;
+    int bind_sock;
+    int len_addr;
+    int listen_sock;
+    int new_sock;
+    long valread;
+    char *hello = "hey from server";
+    
+    if (( fd_server = socket(AF_INET, SOCK_STREAM, 0)) <0)
+    {
+        perror("cannot create socket");
+        return(0);
+    }
+
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    len_addr = sizeof(address);
+
+    bind_sock = bind(fd_server, (struct sockaddr *) &address, len_addr);
+
+    if((listen_sock = listen(fd_server, 10)) < 0)
+    {
+        perror("In listen");
+        return(0);
+    }
+    while(1)
+    {
+        printf("\n waiting for connection");
+        if((new_sock = accept(fd_server, (struct sockaddr *) &address, (socklen_t *)&len_addr)) < 0)
+        {
+            perror("In accept");
+            return(0);
+        }
+        char buffer[30000] = {0};
+        valread = read(new_sock, buffer, 30000);
+        printf("%s\n", buffer);
+        write(new_sock, hello, strlen(hello));
+
+        printf("\n message sent");
+        close(new_sock);
+    }
+    
+}
+
+ 
